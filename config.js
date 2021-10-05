c23716a2151bf515749e1bbce5aa4b288844923b
Vladimir Nevyhosteny <vnevyhosteny@gmail.com>
2021-10-05 13:31:14 +0200

    feat: express server

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  SOURCE_MAP_PATH: './KeweSL.app.dSYM',
+  CRASH_FILE: './crash.crash',
+  MAC_DEVELOPER_DIR: '/Applications/Xcode.app/Contents/Developer',
+  SYMBOLICATECRASH_SCRIPT_PATH: '/Applications/Xcode.app/Contents/SharedFrameworks/DVTFoundation.framework/Versions/A/Resources/symbolicatecrash',
+}
--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
-
-const SOURCE_MAP_PATH = './KeweSL.app.dSYM';
-const CRASH_FILE = './fcrash.crash';
-const MAC_DEVELOPER_DIR = '/Applications/Xcode.app/Contents/Developer'
-const SYMBOLICATECRASH_SCRIPT_PATH = '/Applications/Xcode.app/Contents/SharedFrameworks/DVTFoundation.framework/Versions/A/Resources/symbolicatecrash'
+const config = require('./config');
 
 function composeExecCommand(sourceMap, crash) {
-  return `${SYMBOLICATECRASH_SCRIPT_PATH} --dsym=${sourceMap} ${crash}`
+  return `${config.SYMBOLICATECRASH_SCRIPT_PATH} --dsym=${sourceMap} ${crash}`;
 }
 
 async function symbolicate(sourceMap, crash) {
@@ -16,7 +12,7 @@ async function symbolicate(sourceMap, crash) {
       composeExecCommand(sourceMap, crash),
       {
         env: {
-          DEVELOPER_DIR: MAC_DEVELOPER_DIR,
+          DEVELOPER_DIR: config.MAC_DEVELOPER_DIR,
         },
       },
     );
@@ -25,6 +21,15 @@ async function symbolicate(sourceMap, crash) {
   }
 }
 
-symbolicate(SOURCE_MAP_PATH, CRASH_FILE).then((result) => {
-  console.log(result)
-})
+const express = require('express');
+const app = express();
+
+app.get("/symbolicate", async (req, res, next) => {
+  const result = await symbolicate(config.SOURCE_MAP_PATH, config.CRASH_FILE)
+
+  res.json(result);
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
